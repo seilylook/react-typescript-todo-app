065688df4b3848775117ef2d38d24f721d23ccc4
seilylook <seilylook@naver.com>
2023-02-01 16:02:35 +0900

    view template finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
